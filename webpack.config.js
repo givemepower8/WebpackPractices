cd91fbc33c8c316fb22f4f4dcbaecd9ad029874f
Wei Wang <wei.wang@ia.ca>
2019-03-04 14:48:33 -0800

    Asset Management

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,23 @@ module.exports = {
     contentBase: './dist'
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
   }
 };
